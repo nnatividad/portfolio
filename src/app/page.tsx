10552b068661d3819628759cbbe16ca71657276a
nnatividad <natetividad@gmail.com>
2026-04-14 23:23:34 -0700

    Began home page UI code - seperate the home page into two halves using grid to seperate basic info and component info on right side - image space, basic info, links to github, linkedin, and resume added (no functionality) - completed navbar build and included even spacing and padding to have flush alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,21 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      Welcome to my portfolio
+      <div className={styles.infoContainer}>
+        <div className={styles.temp}/>
+        <div className={styles.info}>
+          <h2>Nate Natividad</h2>
+          <p>B.S in Computer Science from UC Riverside</p>
+          <ul>
+            <li><a href="/"><u>github</u></a></li>
+            <li><a href="/"><u>linkedIn</u></a></li>
+            <li><a href="/"><u>resume</u></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.componentContainer}>
+        Right Side
+      </div>
     </div>
   );
 }
