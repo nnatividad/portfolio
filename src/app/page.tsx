dd80db5298a3dcb4806d20e0cc94df2323e02def
nnatividad <natetividad@gmail.com>
2026-04-16 00:11:40 -0700

    Added a project and styles for project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import profilePic from "../../public/profilePic.jpeg";
+import ASTN from "../../public/ASTN.jpeg";
+import RobsGarments from "../../public/robsGarments.png";
 
 
 export default function Home() {
@@ -31,12 +33,60 @@ export default function Home() {
         </div>
         <div className={styles.experience}>
           <div className={styles.title}><h1>EXPERIENCE</h1></div>
-          <div>
-
+          <div className={styles.experienceContainer}>
+            <ul>
+              <li>
+                <div className={styles.jobInfo}>
+                  <div>
+                    <Image src={ASTN} alt="astn" height={600} width={800} className={styles.image}/>
+                  </div>
+                  <div>
+                    <div className={styles.jobTitle}>
+                      <h2>Full Stack Developer</h2>
+                      <h3>Oct 2024 - Oct 2025</h3>
+                    </div>
+                    <div className={styles.textContainer}>
+                      <p>
+                        Built the MVP architecture for a mobile athlete marketplace from the ground up using React Native, Expo, and Firebase. 
+                        Owned the full development of user onboarding, profile management, and marketplace UI, working closely with product design to deliver an experience aligned with the company's vision. 
+                        Established Git-based version control workflows and documented engineering standards with the broader team. 
+                        Collaborated directly with leadership to translate business requirements into scalable technical solutions.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
         <div className={styles.projects}>
           <div className={styles.title}><h1>PROJECTS</h1></div>
+          <div className={styles.experienceContainer}>
+            <ul>
+              <li>
+                <div className={styles.jobInfo}>
+                  <div>
+                    <Image src={RobsGarments} alt="robsgarments" height={600} width={800} className={styles.image}/>
+                  </div>
+                  <div>
+                    <div className={styles.jobTitle}>
+                      <h2>Robin's Garments</h2>
+                      <div className={styles.projectLinks}>
+                        <a href="https://robs-garments.vercel.app/"><u>site</u></a>
+                        <a href="https://github.com/nnatividad/robs-garments"><u>repo</u></a>
+                      </div>
+                    </div>
+                    <div className={styles.textContainer}>
+                      <p>
+                        A custom e-commerce platform for Robin's Garments. Local to the Bay Area, a business that curates and sells vintage
+                        clothing and sccessories. Built using Next.js, React, Vercel, Sanity, and Stripe.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className={styles.contact}>
           <div className={styles.title}><h1>CONTACT</h1></div>
